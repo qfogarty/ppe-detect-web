9af77b9aa3a24001adfe305d5ff1117bcd1c7a46
fax <fax@fax.com>
2020-12-08 23:47:48 +1300

    added recognition stuff

--- server/index.php
+++ server/index.php
@@ -2,43 +2,93 @@
 
 use Aws\Exception\AwsException;
 use Aws\S3\S3Client;
+use Aws\Rekognition\RekognitionClient;
 
-$s3Client = new S3Client([
-    'version' => 'latest',
-    'region' => $_ENV['S3_REGION'],
-    'credentials' => [
-        'key'    => $_ENV['AWS_ACCESS_KEY_ID'],
-        'secret' => $_ENV['AWS_SECRET_ACCESS_KEY'],
+
+//setup clients
+$config =
+    [
+        'version' => 'latest',
+        'region' => $_ENV['AWS_REGION'],
+        'credentials' => [
+            'key'    => $_ENV['AWS_ACCESS_KEY_ID'],
+            'secret' => $_ENV['AWS_SECRET_ACCESS_KEY'],
+        ],
+        'http'    => [
+            'verify' => __DIR__ . '/../cacert.pem'
+        ]
+    ];
+
+$s3Client = new S3Client($config);
+$rClient = new RekognitionClient($config);
+
+
+//always respond with this
+$response = [
+    "errors" => [],
+    "detected" => []
+];
+
+if (!isset($_FILES['image']) || !@getimagesize($_FILES['image']['tmp_name'])) {
+    http_response_code(422);
+    $response['errors'][] = 'Image is required!';
+    echo json_encode($response);
+    return;
+}
+
+$file_name = $_FILES['image']['name'];
+$temp_file_location = $_FILES['image']['tmp_name'];
+$s3Object = $s3Client->putObject([
+    'Bucket' => $_ENV['S3_BUCKET'],
+    'Key'    => $file_name,
+    'SourceFile' => $temp_file_location
+]);
+
+//detect all the things
+$result = $rClient->detectProtectiveEquipment([
+    'Image' => [
+        'S3Object' => [
+            'Bucket' => $_ENV['S3_BUCKET'],
+            'Name' => $file_name
+        ],
     ],
-    'http'    => [
-        'verify' => __DIR__ . '/../cacert.pem'
+    'SummarizationAttributes' => [
+        'MinConfidence' => 0, // REQUIRED
+        'RequiredEquipmentTypes' => ['FACE_COVER'], // REQUIRED
     ]
 ]);
 
-if (isset($_FILES['image'])) {
-    $file_name = $_FILES['image']['name'];
-    $temp_file_location = $_FILES['image']['tmp_name'];
+//try and format a reponse :D
+try {
+    foreach ($result['Persons'] as $person) {
+        foreach ($person['BodyParts'] as $bodypart) {
+            //only keep faces
+            if ($bodypart['Name'] != 'FACE') continue;
 
-    $result = $s3Client->putObject([
-        'Bucket' => $_ENV['S3_BUCKET'],
-        'Key'    => $file_name,
-        'SourceFile' => $temp_file_locatione
-    ]);
+            //we construct a face
+            $face = ['type' => 'FACE', 'cover' => false];
+            $response['detected'][] = &$face;
+
+            //only keep items that have EquipmentDetections
+            if (!isset($bodypart['EquipmentDetections']) || empty($bodypart['EquipmentDetections'])) continue;
+
+            //gotsta have the quipment
+            foreach ($bodypart['EquipmentDetections'] as $equipment) {
+                // //they gotta face hugger?
+                if ($equipment['Type'] == 'FACE_COVER' && isset($equipment['CoversBodyPart'])) {
+                    $face['cover'] = true;
+                }
+            }
+        }
+    }
+} catch (\Throwable $th) {
+    http_response_code(500);
+    $response['errors'][] = 'Something went wrong';
 }
 
-// /https://docs.aws.amazon.com/aws-sdk-php/v3/api/api-rekognition-2016-06-27.html#detectprotectiveequipment
+if (empty($response['detected'])) {
+    $response['errors'][] = 'No Body parts found';
+}
 
-// $result = $client->detectProtectiveEquipment([
-//     'Image' => [ // REQUIRED
-//         'Bytes' => <string || resource || Psr\Http\Message\StreamInterface>,
-//         'S3Object' => [
-//             'Bucket' => '<string>',
-//             'Name' => '<string>',
-//             'Version' => '<string>',
-//         ],
-//     ],
-//     'SummarizationAttributes' => [
-//         'MinConfidence' => <float>, // REQUIRED
-//         'RequiredEquipmentTypes' => ['<string>', ...], // REQUIRED
-//     ],
-// ]);
+// var_dump($result);
+echo json_encode($response);
